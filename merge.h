a64ed9c4a9d0727f53ca8d741edafa2e20184d5b
Chase Hutton <chasehutton6@gmail.com>
2025-01-24 21:26:53 -0500

    small changes

--- merge.h
+++ merge.h
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <random>
 #include <iostream>
+#include <chrono>
+
 
 #include "utils.h"
 #include "parlay/sequence.h"
@@ -317,13 +319,24 @@ void Merge(parlay::sequence<uint32_t>& seq, uint32_t b) {
 
     bool* flag = (bool*) std::malloc(sizeof(bool));
     *flag = false;
-
     std::cout << "Setting Up...\n\n";
+    auto start = std::chrono::high_resolution_clock().now();
     SetUp(seq, b);
+    auto end = std::chrono::high_resolution_clock().now();
+    auto time = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    std::cout << "Time for SetUp: " << time.count() << " \n";
     std::cout << "End Sorting...\n\n";
+    start = std::chrono::high_resolution_clock().now();
     EndSort(seq, b, flag);
+    end = std::chrono::high_resolution_clock().now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    std::cout << "Time for EndSort: " << time.count() << " \n";
     std::cout << "Seq Sorting...\n\n";
+    start = std::chrono::high_resolution_clock().now();
     SeqSort(seq, 0, (uint32_t)seq.size(), b);
+    end = std::chrono::high_resolution_clock().now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    std::cout << "Time for SeqSort: " << time.count() << " \n";
 
     std::free(flag);
 }
